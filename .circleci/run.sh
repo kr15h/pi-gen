d5d5848f0ffadbb478c89c5a4da8e1d760d7561d
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2019-03-13 23:17:04 +0200

    Attempt to fix touches in ci/run

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -7,13 +7,13 @@ apt-get update
 apt-get -y install coreutils quilt parted qemu-user-static debootstrap zerofree pxz zip dosfstools bsdtar libcap2-bin grep rsync xz-utils file git curl xxd kmod
 touch config
 echo 'IMG_NAME=opm' >> config
-if [ -f ../stage0/EXPORT_NOOBS ]; then rm ../stage0/EXPORT_NOOBS; fi
-if [ -f ../stage1/EXPORT_NOOBS ]; then rm ../stage1/EXPORT_NOOBS; fi
-if [ -f ../stage2/EXPORT_NOOBS ]; then rm ../stage2/EXPORT_NOOBS; fi
-if [ -f ../stage0/EXPORT_IMAGE ]; then rm ../stage0/EXPORT_IMAGE; fi
-if [ -f ../stage1/EXPORT_IMAGE ]; then rm ../stage1/EXPORT_IMAGE; fi
-if [ -f ../stage2/EXPORT_IMAGE ]; then rm ../stage2/EXPORT_IMAGE; fi
-touch ../stage4/SKIP ../stage5/SKIP
-touch ../stage4/SKIP_IMAGES ../stage5/SKIP_IMAGES
-touch ../stage3/EXPORT_IMAGE
+if [ -f ./stage0/EXPORT_NOOBS ]; then rm ./stage0/EXPORT_NOOBS; fi
+if [ -f ./stage1/EXPORT_NOOBS ]; then rm ./stage1/EXPORT_NOOBS; fi
+if [ -f ./stage2/EXPORT_NOOBS ]; then rm ./stage2/EXPORT_NOOBS; fi
+if [ -f ./stage0/EXPORT_IMAGE ]; then rm ./stage0/EXPORT_IMAGE; fi
+if [ -f ./stage1/EXPORT_IMAGE ]; then rm ./stage1/EXPORT_IMAGE; fi
+if [ -f ./stage2/EXPORT_IMAGE ]; then rm ./stage2/EXPORT_IMAGE; fi
+touch ./stage4/SKIP ./stage5/SKIP
+touch ./stage4/SKIP_IMAGES ./stage5/SKIP_IMAGES
+touch ./stage3/EXPORT_IMAGE
 ./build.sh
